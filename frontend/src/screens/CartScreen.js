8251172eb0d8e8040a20cbc1f7b24144ca3b2c70
Parvesh Barak <pbarak6@gmail.com>
2022-04-20 07:40:21 +0530

    Shopping cart-2

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
@@ -11,18 +11,26 @@ const CartScreen = () => {
   const location = useLocation
   const navigate = useNavigate
 
-  const qty = location.search ? Number(location.search.split('=')[1]) : 1
-
+  const qty = location.search ? Number(location.search.split('=')[1]) : 3
+  console.log(qty)
   const dispatch = useDispatch()
 
-  const cart = useSelector((state) => state.cart)
+  const cart = useSelector(state => state.cart)
   const { cartItems } = cart
+
+  console.log(cart)
 
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
+
+  const removeFromCartHandler = (id) => {}
+
+  const checkoutHandler = () => {
+    navigate('/login?redirect=shipping')
+  }
 
   return (
     <Row>
@@ -33,11 +41,74 @@ const CartScreen = () => {
             Your Cart is Empty! <Link to='/'>Go Back</Link>{' '}
           </Message>
         ) : (
-          <ListGroup variant='flush'></ListGroup>
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      fluid
+                      rounded
+                    ></Image>
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
         )}
       </Col>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                {' '}
+                SubTotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                Items
+              </h2>
+              ${' '}
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroupItem>
+              <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler} >Proceed to Checkout </Button>
+            </ListGroupItem>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
   )
 }
